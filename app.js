5af122f3183015c44c41cf815765e5c291707969
prmfi0 <parhamhp18@gmail.com>
2026-06-18 22:23:01 +0300

    prmfi0 : add more media , add post option

--- app.js
+++ app.js
@@ -3,3 +3,45 @@ const toggleBtn = document.getElementById("theme-toggle");
 toggleBtn.addEventListener("click", () => {
   document.body.classList.toggle("dark");
 });
+
+const addbtn = document.querySelector('.send-task');
+const gettext = document.querySelector('.input-task');
+const tasks = document.querySelector('#tasks');
+
+addbtn.addEventListener('click', function () {
+
+  const inputvalue = gettext.value;
+
+  if (inputvalue.trim() === '') return;
+
+  const div = document.createElement('div');
+
+  div.classList.add('box');
+
+  div.innerHTML = `
+    <input type="radio" class="check-task">
+    <p class="text-task">${inputvalue}</p>
+
+    <div class="buttons">
+      <button class="delete-icon">
+        <img class="icon" src="delete.svg">
+      </button>
+
+      <button class="edit">
+        <img class="icon" src="edit.svg">
+      </button>
+    </div>
+  `;
+  tasks.appendChild(div);
+  gettext.value = '';
+});
+tasks.addEventListener("click", function(event) {
+
+    if (event.target.closest(".delete-icon")) {
+
+        const currentTask = event.target.closest(".box");
+
+        currentTask.remove();
+    }
+
+});
